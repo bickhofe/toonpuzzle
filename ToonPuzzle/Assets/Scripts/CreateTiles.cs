3e096655c8c61ffa8c9f6ed42006e708bd2665db
Peter Bickhofe <peter@bickhofe.de>
2016-03-18 16:35:55 +0100

    1st commit

---
+++ ToonPuzzle/Assets/Scripts/CreateTiles.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTiles : MonoBehaviour {
+
+	public int maxTiles;
+	public GameObject[] Tiles;
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i<10; i++){
+			Debug.Log(i);
+			GameObject myTile = Instantiate(Tiles[Random.Range(0,5)], new Vector3(i*6.1f, 0, 0), transform.rotation) as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
