0678e96b53ecffc41f586ec820dc4d19fe2d7f39
bickhofe <peter@bickhofe.de>
2016-03-19 13:07:53 +0100

    tile movement

--- ToonPuzzle/Assets/Scripts/CreateTiles.cs
+++ ToonPuzzle/Assets/Scripts/CreateTiles.cs
@@ -1,21 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CreateTiles : MonoBehaviour {
 
 	public int maxTiles;
 	public GameObject[] Tiles;
+	public GameObject Container;
+	public GameObject Buttons;
+	public int row = 0;
+	public int col = 0;
+	float xOffset = 6.1f;
+	float yOffset = 6.65f;
 
-	// Use this for initialization
+	public List<Vector2> tilePos = new List<Vector2>();
+
 	void Start () {
-		for (int i=0; i<10; i++){
-			Debug.Log(i);
-			GameObject myTile = Instantiate(Tiles[Random.Range(0,5)], new Vector3(i*6.1f, 0, 0), transform.rotation) as GameObject;
-		}
+		Buttons = GameObject.Find ("Buttons");
+		Container = GameObject.Find ("PuzzleContainer");
+		CreateTile (0,0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		AdjustCam ();
+	}
+
+	void AdjustCam(){
+		transform.position = new Vector3 (transform.rotation.y*20, transform.rotation.x*-10, -5);
+	}
+
+	void ShowHideButtons(){
+		//toggle buttons visibility
+		if (Buttons.activeSelf) Buttons.SetActive (false);
+		else Buttons.SetActive (true);
+	}
+
+	public void CreateTile(int rowVal,int colVal) {
+		Vector2 pos = new Vector2(row,col);
+		tilePos.Add(pos);
+		print (tilePos);
+
+		row += rowVal;
+		col += colVal;
+		//print ("row: " + row + " " + "col: " + col);
+		GameObject myTile = Instantiate(Tiles[Random.Range(0,5)], new Vector3(rowVal*xOffset, colVal*yOffset, 10), Quaternion.identity) as GameObject;
+		iTween.MoveTo(myTile,iTween.Hash("z",0,"time",1,"delay",0,"islocal",true,"easetype",iTween.EaseType.easeOutBounce));	
+		myTile.transform.parent = Container.transform;
+		//Container.transform.position = new Vector3(row*-offset, col*-offset, 0);
+		iTween.MoveTo(Container,iTween.Hash("position",new Vector3(row*-xOffset, col*-yOffset, 0),"time",1,"delay",0,"easetype",iTween.EaseType.easeInOutSine));	
+		//ShowHideButtons ();
 	}
 }
---
+++ ToonPuzzle/Assets/Scripts/RayCast.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RayCast : MonoBehaviour {
+
+	public CreateTiles TilesScript;
+	GameObject curButton;
+	RaycastHit hit;
+	float time = 0;
+
+	// Use this for initialization
+	void Start () {
+		TilesScript = GetComponent<CreateTiles> ();
+	}
+
+	void FixedUpdate() 
+	{
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		//Debug.DrawRay(transform.position, fwd*25, Color.green);
+
+		if (Physics.Raycast (transform.position, fwd, out hit)) {
+			if (hit.collider.name == "plusRight") NewTile(1,0);
+			else if (hit.collider.name == "plusLeft") NewTile(-1,0);
+			else if (hit.collider.name == "plusTop") NewTile(0,1);
+			else if (hit.collider.name == "plusBottom") NewTile(0,-1);
+		} else {
+			if (curButton != null) curButton.transform.localScale = new Vector3(.5f,.5f,.5f);
+		}
+	}
+
+	void NewTile(int row, int col){
+
+		curButton = hit.collider.gameObject;
+		time += Time.deltaTime;
+		curButton.transform.localScale = new Vector3(.75f,.75f,.75f);
+		//delay
+		if (time > 3) {
+			curButton.transform.localScale = new Vector3(.5f,.5f,.5f);
+			TilesScript.CreateTile (row, col);
+			time = 0;
+		}
+	}
+
+}
